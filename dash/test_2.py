e67854a0d8a901e31384b48d243222023903c630
DLIERBA <38899068+DLIERBA@users.noreply.github.com>
2020-01-05 21:21:45 +0800

    dash

---
+++ dash/test_2.py
@@ -0,0 +1,50 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv('test_2.csv')
+
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                dict(
+                    x=df[df['continent'] == i]['Children ages 0-14 newly infected with HIV'],
+                    y=df[df['continent'] == i]['Adults ages15 newly infected with HIV'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': dict(
+                xaxis={'type': 'log', 'title': 'Children ages 0-14 newly infected with HIV'},
+                yaxis={'title': 'Adults ages15 newly infected with HIV'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
